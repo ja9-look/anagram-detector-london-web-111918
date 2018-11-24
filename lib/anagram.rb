f33891aa1656b8faceeabb27a41f9288cb669de3
Janine Luk <janineluk95@gmail.com>
2018-11-24 22:01:25 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word 
+  end
+  
+  def match(array)
+    array.each_with_object([]) {|check, anagrams|
+      anagrams << check if word.split("").sort.join == check.split("").sort.join}
+      
+  end
+end
